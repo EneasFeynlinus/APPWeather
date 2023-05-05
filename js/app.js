49193035c063baade3b13a02a55f10798a9ee51a
Eneas Junior <eneas.silva.jr@gmail.com>
2023-05-05 12:18:19 -0300

    Finished adding images to the application, using properties from API object and manipulating elements from DOM

--- js/app.js
+++ js/app.js
@@ -3,18 +3,31 @@ const cityNameContainer = document.querySelector('[data-js="city-name"]')
 const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
 const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]')
 const cityCard = document.querySelector('[data-js="city-card"]')
+const timeImg = document.querySelector('[data-js="time"]')
+const timeIconContainer = document.querySelector('[data-js="time-icon"]')
+
 cityForm.addEventListener('submit', async event => {
     event.preventDefault()
 
     const inputValue = event.target.city.value
 
     const [{ LocalizedName, Key }] = await getCityData(inputValue)
-    const [{ WeatherText, Temperature }] = await getCityWeather(Key)
+    const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
 
+    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
     if (cityCard.classList.contains('d-none')) {
         cityCard.classList.remove('d-none')
     }
 
+    if (IsDayTime) {
+        timeImg.src = './src/day.svg'
+        console.log('It is day')
+    } else {
+        timeImg.src = './src/night.svg'
+        console.log('It is nuight')
+    }
+
+    timeIconContainer.innerHTML = timeIcon
     cityNameContainer.textContent = LocalizedName
     cityWeatherContainer.textContent = WeatherText
     cityTemperatureContainer.textContent = Temperature.Metric.Value
@@ -22,3 +35,4 @@ cityForm.addEventListener('submit', async event => {
     console.log(WeatherText, Temperature.Metric.Value)
     cityForm.reset()
 })
+
