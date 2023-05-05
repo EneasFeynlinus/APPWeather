8002bb538a8ff26d0d2bd71da86bbb08a42553b0
Eneas Junior <eneas.silva.jr@gmail.com>
2023-05-05 10:32:56 -0300

    Refatoring the functions to make fetch function more generic.

--- js/weather.js
+++ js/weather.js
@@ -2,38 +2,42 @@ const APIKey = 'BgtuUrTT2I38R0K9MghVtXnxfs0v3jSe'
 const getCityUrl = cityName =>
     `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
 
-const getCityData = async cityName => {
+const fetchData = async url => {
     try {
-        const cityUrl = getCityUrl(cityName)
-        const response = await fetch(cityUrl)
+        const response = await fetch(url)
 
         if (!response.ok) {
             throw new Error('Data could not be reached.')
         }
 
-        const [cityData] = await response.json()
-        return cityData
+        return await response.json()
     } catch ({ name, message }) {
         alert(`${name}: ${message}`)
     }
 }
 
-const getWeatherData = async cityName => {
-    try {
-        const { Key } = await getCityData(cityName)
-        const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br`
-        const response = await fetch(weatherUrl)
+const getCityData = cityName => fetchData(getCityUrl(cityName))
 
-        if (!response.ok) {
-            throw new Error('Data could not be reached.')
-        }
 
-        const weatherData = await response.json()
-        return weatherData
-    } catch ({ name, message }) {
-        alert(`${name}: ${message}`)
-    }
-}
-
-getWeatherData('São Paulo')
+getCityData('São Paulo')
     .then(console.log)
+
+// const getWeatherData = async cityName => {
+//     try {
+//         const { Key } = await getCityData(cityName)
+//         const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br`
+//         const response = await fetch(weatherUrl)
+
+//         if (!response.ok) {
+//             throw new Error('Data could not be reached.')
+//         }
+
+//         const weatherData = await response.json()
+//         return weatherData
+//     } catch ({ name, message }) {
+//         alert(`${name}: ${message}`)
+//     }
+// }
+
+// getWeatherData('São Paulo')
+//     .then(console.log)
