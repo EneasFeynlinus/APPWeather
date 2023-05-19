c63532f32ec8af29e12105dfdcc3e138b02ed312
Eneas Junior <eneas.silva.jr@gmail.com>
2023-05-19 10:49:45 -0300

    Correcting the https from baseUrl

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 const APIKey = 'BgtuUrTT2I38R0K9MghVtXnxfs0v3jSe'
-const baseUrl = 'http://dataservice.accuweather.com/'
+const baseUrl = 'https://dataservice.accuweather.com/'
 
 const getCityUrl = cityName =>
     `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
